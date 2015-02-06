64939b2f18d16b89753a3ab5f297c4afd712fc0c
Pip3r4o <pip3r4o@gmail.com>
2015-02-06 16:17:34 +0200

    Added solution to problem 6 - Maximal Sum of K elements

---
+++ CSharp-Part-2-Homework-Assignments/01.Arrays Homework/06.MaximalKSum/06.MaximalKSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+//Write a program that reads two integer numbers N and K and an array of N elements from the console.
+//Find in the array those K elements that have maximal sum.
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the N parameter: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n You are looking for the K(number of elements) that sum up to the max possible sum of K numbers in the array");
+            Console.WriteLine("Enter the K parameter: ");
+            int K = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int[] arr = new int[N];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Please input a number for arr[i]: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            long maxSum = arr[arr.Length - 1] + arr[arr.Length - 2];
+            Console.WriteLine("The maximal possible sum of the number of elements (K = {0}) is {1}.", K, maxSum);
+
+
+        }
+    }
+
