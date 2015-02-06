a138620c418a2c0d36f95e9eee974abaa3be8c50
Pip3r4o <pip3r4o@gmail.com>
2015-02-06 14:59:26 +0200

    Added solution to problem 04. Maximal Sequence

---
+++ CSharp-Part-2-Homework-Assignments/01.Arrays Homework/04.MaximalSequence/04.MaximalSequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//Write a program that finds the maximal sequence of equal elements in an array.
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 1, 1, 3, 4, 5, 5, 5, 5, 5, 3, 3, 4, 3, 3 };  //The longest sequence of equal chars is 5 of 5s.
+                    // Alternative array { 2, 1, 1, 2, 3, 3, 2, 2, 2, 1 };
+
+        int frequency = 1;
+        int longestSequence = 0;
+        int numberOfLongestSequence = 0;
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] != arr[i - 1])
+            {
+                frequency = 1;
+            }
+            else
+            {
+                ++frequency;
+                if (frequency > longestSequence)
+                {
+                    longestSequence = frequency;
+                    numberOfLongestSequence = arr[i];
+                }
+            }
+        }
+
+        Console.WriteLine("The length of the longest sequence of equal elements is: " + longestSequence);
+        Console.WriteLine(numberOfLongestSequence);
+    }
+}
+
