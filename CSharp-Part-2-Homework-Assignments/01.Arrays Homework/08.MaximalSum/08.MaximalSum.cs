9842170b56aaa02ed76b672a9cc8649c892291ef
Pip3r4o <pip3r4o@gmail.com>
2015-02-07 13:47:19 +0200

    Added solutions to task 8 and 9

---
+++ CSharp-Part-2-Homework-Assignments/01.Arrays Homework/08.MaximalSum/08.MaximalSum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Write a program that finds the sequence of maximal sum in given array.
+//                            Example:
+
+//           input	                    result
+//2, 3, -6, -1, 2, -1, 6, 4, -8, 8	  2, -1, 6, 4 = 11
+//Can you do it with only one loop (with single scan through the elements of the array)?
+
+class Program
+{
+    static void Main()
+    {
+        /*Console.Write("Enter the number of elements your array will contain: "); //<----------  Decomment these lines to enter your own array
+        int N = int.Parse(Console.ReadLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        } */
+
+        int[] arr = { 2, 3, -6, -1, 2, -1, 6, 4, -8, 8 };
+        int currentSum = 0;
+        int maxSum = 0;
+
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            currentSum = arr[i] + currentSum;
+            
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0)
+            {
+                currentSum = 0;
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("The maximal sum of a subarray is: {0}", maxSum);
+        Console.WriteLine();
+    }
+}
---
+++ CSharp-Part-2-Homework-Assignments/01.Arrays Homework/09.FrequentNumber/09.FrequentNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Write a program that finds the most frequent number in an array.
+
+class Program
+{
+    static void Main()
+    {
+        /*Console.Write("Enter the number of elements your array will contain: "); //<----------  Decomment these lines to enter your own array
+        int N = int.Parse(Console.ReadLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        } */
+
+        int[] arr = { 4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3 }; // Supposed output 4 (5 times)
+        int currentFrequency = 0;
+        int largestFrequency = 0;
+        int num = 0;
+        int mostPopNumber = 0;
+
+        Array.Sort(arr);
+        for (int i = 0; i < arr.Length-1; i++)
+        {
+            if (num == arr[i] || i == 0)
+            {
+                num = arr[i];
+                ++currentFrequency;
+            }
+
+            if (currentFrequency > largestFrequency)
+            {
+                largestFrequency = currentFrequency;
+                mostPopNumber = arr[i];
+            }
+            if (arr.Length-1 > i && arr[i + 1] != num)
+            {
+                num = arr[i + 1];
+                currentFrequency = 0;
+            }
+        }
+
+        Console.WriteLine("The most popular number in the integer array is {0}, with {1} occurences.", mostPopNumber, largestFrequency);
+    }
+}
