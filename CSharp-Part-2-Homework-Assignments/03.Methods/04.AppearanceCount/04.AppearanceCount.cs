5cbb3d5a9a00b0e3c7de88953301e66cf041af4d
pip3r4o <pip3r4o@gmail.com>
2015-02-13 20:27:23 +0200

    Telerik Academy - Course 2 - CSharp 2 - Solution to Task 4

--- CSharp-Part-2-Homework-Assignments/03.Methods/04.AppearanceCount/04.AppearanceCount.cs
+++ CSharp-Part-2-Homework-Assignments/03.Methods/04.AppearanceCount/04.AppearanceCount.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 //Write a method that counts how many times given number appears in given array.
-//Write a test program to check if the method is workings correctly.
+//Write a test program to check if the method is workings correctly..
 
 class Program
 {
