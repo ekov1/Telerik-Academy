bb962d7d70ddc1e57704c45c85868dc73318e048
Pip3r4o <pip3r4o@gmail.com>
2015-02-27 17:23:40 +0200

    Telerik Academy - Course 2 - CSharp 2 - Solutions to Tasks 1 and 2

---
+++ CSharp-Part-2-Homework-Assignments/08.TextFiles/01.OddLines/01.OddLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program that reads a text file and prints on the console its odd lines.
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("The following text makes no sense because only every other line is displayed!\n\n");
+        //string path = Console.ReadLine();
+
+        StreamReader reader = new StreamReader("..\\..\\test.txt");
+        string[] fileContents = reader.ReadToEnd().Split('\n');
+
+        var result = new StringBuilder();
+
+        for (int line = 0; line < fileContents.Length; line += 2)
+        {
+            result.AppendLine(fileContents[line]);
+        }
+
+        Console.WriteLine(result);
+        reader.Close();
+    }
+}
+
