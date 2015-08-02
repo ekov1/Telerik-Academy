0a0f538b623204413b0690ee25b0c12938add2af
Pip3r4o <pip3r4o@gmail.com>
2015-08-02 22:25:31 +0300

    JS UI and DOM Homework Assignment

--- Javascript-UI-DOM/07.DOM Operations/tasks/task-1.js
+++ Javascript-UI-DOM/07.DOM Operations/tasks/task-1.js
@@ -1,6 +1,6 @@
 /* globals $ */
 
-/* 
+/*
 
 Create a function that takes an id or DOM element and an array of contents
 
@@ -14,12 +14,44 @@ Create a function that takes an id or DOM element and an array of contents
   * Any of the function params is missing
   * Any of the function params is not as described
   * Any of the contents is neight `string` or `number`
-    * In that case, the content of the element **must not be** changed   
+    * In that case, the content of the element **must not be** changed
 */
 
 module.exports = function () {
 
   return function (element, contents) {
-  	throw new Error('Pesho');
+
+  	if (!contents || isNaN(contents.length)) {
+  		throw new Error('Contents is not a valid object');
+  	}
+  	var contsLen = contents.length,
+  		i,
+  		frag = document.createDocumentFragment(),
+  		div = document.createElement('div');
+
+  	contents = [].slice.call(contents);
+
+    if (typeof(element) === 'string') {
+    	element = document.getElementById(element);
+    	if (!element) {
+    		throw new Error('No element with the provided id!');
+    	}
+    } else if (!(element instanceof HTMLElement)) {
+    	throw new Error('Not a valid HTML element!');
+    }
+
+    for (i = 0; i < contsLen; i+= 1) {
+    	var content = contents[i],
+    		currentDiv = div.cloneNode(true);
+    	if (typeof(content) !== 'string' && typeof(content) !== 'number') {
+    		throw new Error('Invalid content!');
+    	}
+
+    	currentDiv.innerHTML = content;
+    	frag.appendChild(currentDiv);
+    };
+
+    element.innerHTML = '';
+    element.appendChild(frag);
   };
 };
