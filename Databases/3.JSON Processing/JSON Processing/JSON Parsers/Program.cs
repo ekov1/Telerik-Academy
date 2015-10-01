0aaa0c84914bd0f66a28f39f5ab502ddf892b467
Pip3r4o <pip3r4o@gmail.com>
2015-10-01 23:08:59 +0300

    JSON Processing .NET Homework Assignment

---
+++ Databases/3.JSON Processing/JSON Processing/JSON Parsers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JSON_Parsers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var data = new WebClient().DownloadData("https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw");
+            var rssString = Encoding.UTF8.GetString(data).TrimEnd('\0');
+
+            var doc = XDocument.Parse(rssString);
+            
+            doc.Save("../../files/ta-yt-feed.xml");
+
+            var json = JsonConvert.SerializeXNode(doc, Formatting.Indented);
+
+            File.WriteAllText("../../files/ta-yt-feed.json", json);
+
+            var jsonObj = JObject.Parse(json);
+            var entries = jsonObj["feed"]["entry"]
+                .Select(y => y["title"]).ToList();
+
+            Console.WriteLine("Video Titles found in the RSS Feed: \n" + string.Join("\r\n", entries) + "\n");
+            Console.WriteLine("--------------");
+        }
+    }
+}
