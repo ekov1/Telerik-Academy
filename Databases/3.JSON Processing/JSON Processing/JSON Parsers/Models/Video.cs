054b6907656dc5190affadaa912b13a3401e88e0
Pip3r4o <pip3r4o@gmail.com>
2015-10-03 16:41:19 +0300

    JSON Processing .NET Homework Assignment

---
+++ Databases/3.JSON Processing/JSON Processing/JSON Parsers/Models/Video.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace JSON_Parsers.Models
+{
+    public class Video
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("link")]
+        public Url Link { get; set; }
+
+        [JsonProperty("yt:videoId")]
+        public string Id { get; set; }
+    }
+}
--- Databases/3.JSON Processing/JSON Processing/JSON Parsers/Program.cs
+++ Databases/3.JSON Processing/JSON Processing/JSON Parsers/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Xml.Linq;
+using JSON_Parsers.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -17,19 +19,79 @@ namespace JSON_Parsers
             var rssString = Encoding.UTF8.GetString(data).TrimEnd('\0');
 
             var doc = XDocument.Parse(rssString);
-            
-            doc.Save("../../files/ta-yt-feed.xml");
+
+            doc.Save("../../Output Files/ta-yt-feed.xml");
 
             var json = JsonConvert.SerializeXNode(doc, Formatting.Indented);
 
-            File.WriteAllText("../../files/ta-yt-feed.json", json);
-
             var jsonObj = JObject.Parse(json);
-            var entries = jsonObj["feed"]["entry"]
+
+            File.WriteAllText("../../Output Files/ta-yt-feed.json", json);
+
+            GetAllVideoTitles(jsonObj);
+
+            var videos = GetAllVideosFromJSON(jsonObj);
+            videos = MakeValidEmbedableVideoLinks(videos);
+            var html = GenerateHTMLFromVideos(videos);
+
+            File.WriteAllText("../../Output Files/parsedFeed.html", html);
+
+        }
+
+        private static void GetAllVideoTitles(JObject json)
+        {
+            var entries = json["feed"]["entry"]
                 .Select(y => y["title"]).ToList();
 
             Console.WriteLine("Video Titles found in the RSS Feed: \n" + string.Join("\r\n", entries) + "\n");
             Console.WriteLine("--------------");
         }
+
+        private static IEnumerable<Video> GetAllVideosFromJSON(JObject json)
+        {
+            return json["feed"]["entry"].Select(x => JsonConvert.DeserializeObject<Video>(x.ToString()));
+        }
+
+        private static IEnumerable<Video> MakeValidEmbedableVideoLinks(IEnumerable<Video> videos)
+        {
+            var makeValidEmbedableVideoLinks = videos as Video[] ?? videos.ToArray();
+
+            foreach (var video in makeValidEmbedableVideoLinks)
+            {
+                video.Link.Href = video.Link.Href.Replace("watch?v=", "embed/");
+            }
+
+            return makeValidEmbedableVideoLinks;
+        }
+
+        private static string GenerateHTMLFromVideos(IEnumerable<Video> videos)
+        {
+            var html = new StringBuilder();
+
+            html.Append("<!DOCTYPE html><html>" +
+                "<head>" +
+                "<style>" + 
+                "body { " +
+                        "background-color:#00FF22;" +
+                        "}" +
+                    "div { " +
+                        "width: 420px; height: 450px; padding:10px; margin:5px; background-color:#AAA; border-radius:10px " +
+                        "}" +
+                "</style>" +
+                "</head>" +
+                "<body>");
+
+            foreach (var video in videos)
+            {
+                html.Append("<div> <h4>" + video.Title + "</h4>" +
+                                  "<iframe width=\"420\" height=\"345\" " + "src=\"" + video.Link.Href + "\" " +
+                                  "frameborder=\"0\" allowfullscreen></iframe>" +
+                                  "<br /><a href=\"" + video.Link.Href + "\">Watch on Youtube</a></div>");
+            }
+            html.Append("</body></html>");
+
+            return html.ToString();
+        }
+
     }
 }
