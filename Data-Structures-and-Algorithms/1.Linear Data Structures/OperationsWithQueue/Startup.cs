86bd142be0b65a33456d0adfd6239276f5512ed1
Pip3r4o <pip3r4o@gmail.com>
2015-10-28 22:32:13 +0200

    10th task

--- Data-Structures-and-Algorithms/1.Linear Data Structures/OperationsWithQueue/Startup.cs
+++ Data-Structures-and-Algorithms/1.Linear Data Structures/OperationsWithQueue/Startup.cs
@@ -2,23 +2,46 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public static class Startup
     {
         public static void Main()
         {
-            var operations = new List<Func<int, int>>
-            {
-                x => x + 1,
-                x => x + 2,
-                x => x * 2
-            };
-
-            var n = 3;
-            var m = 15;
+            var start = 2;
+            var end = 53;
 
             var q = new Queue<int>();
 
+            while (start <= end)
+            {
+                q.Enqueue(end);
+
+                if (end / 2 >= start)
+                {
+                    if (end % 2 == 0)
+                    {
+                        end /= 2;
+                    }
+                    else
+                    {
+                        end--;
+                    }
+                }
+                else
+                {
+                    if (end - 2 >= start)
+                    {
+                        end -= 2;
+                    }
+                    else
+                    {
+                        end--;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" -> ", q.Reverse()));
         }
     }
 }
